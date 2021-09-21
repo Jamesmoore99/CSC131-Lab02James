642b929c95a73b036aaa0c3bde720888b6223d28
James Megacomp <James Megacomp@10.0.0.209>
2021-09-20 19:15:24 -0700

    Added SacStateID - James

--- RepositoryTest/Hello.java
+++ RepositoryTest/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 		System.out.println("Welcome to Git!");
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
-		System.out.println("SacStateID - This line was added by James Moore.");
+		System.out.println("302464734 - This line was added by James Moore.");
 		System.out.println("SacStateID - This line was added by Tyler Ito.");
 		System.out.println("SacStateID - This line was added by Fernando Ramos.");
 		System.out.println("301565654 - This line was added by Samson Lee.");
